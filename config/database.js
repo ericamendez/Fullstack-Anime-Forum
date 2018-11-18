2ee30048d4fc7364c0e3af2f30a50dee97114ed0
ericamendez <ecmendez25@gmail.com>
2018-11-18 13:21:28 -0500

    first version

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://natsu:dragon1@ds161856.mlab.com:61856/anime' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
